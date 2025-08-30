bbe2ae065656d2a8ac72d34316daa305e5129983
srgrenesko <srgrenesko@gmail.com>
2025-08-30 17:17:31 -0400

    Added servo functionality, adjusted LED function, edited readme

--- myo_utilities.h
+++ myo_utilities.h
@@ -1,12 +1,12 @@
 /*
 Myoelectric Utilities Library by Stephen Grenesko
 
-This library contains a variety of pre made functions in order to 
+This library contains a variety of pre made functions in order to
 streamline the software used by microcontrollers in conjunction
-with myoelectric sensors. 
+with myoelectric sensors.
 This library will allow for cleaner code practices,
  as well as creating a layer of abstraction to remove
-the need of tedious serial porting and pin setup. 
+the need of tedious serial porting and pin setup.
 Supports more simplistic interaction between the sensor and standard controlled hardware.
 
 */
@@ -14,19 +14,26 @@ Supports more simplistic interaction between the sensor and standard controlled 
 #define MYO_UTILITIES_H
 
 #include "Arduino.h"
+#include <Servo.h>
 
-class MyoUtil {
-  public:
-    MyoUtil(int pin);       //Constructor for EMG pin
-    void begin(long baud=115200); //Serial init
-    void serialPlot();      //Stream EMG to Serial Plotter
-    int readRaw();          //Return raw EMG reading
-    void ledAction(bool ledState, int ledPin);
+class MyoUtil
+{
+public:
+  MyoUtil(int pin);               //Constructor for EMG pin
+  void begin(long baud = 115200); //Serial init
+  void serialPlot();              //Stream EMG to Serial Plotter
+  int readRaw();                  //Return raw EMG reading
+  void ledToggle(bool ledState, int ledPin, int threshold);    //Control an LED based on muscle activity
+  void servoInit(int servoPin, int maxAngle);   //Servo init
+  void servoUpdate();                           //Update servo position based on muscle activity
 
-  private:
-    int _pin;
-    int _ledPin;
-    bool _ledState;
+private:
+  int _pin;
+  int _ledPin;
+  bool _ledState;
+  int _servoPin;
+  int _maxAngle;
+  Servo servo;
 };
 
 #endif
